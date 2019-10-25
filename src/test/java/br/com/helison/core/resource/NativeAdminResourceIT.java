e3598b516f7d76b92946c98516b9d3d33c706e6d
Rodrigo Freitas <rodrigo.freitas@tecsinapse.com.br>
2019-10-25 20:18:28 -0300

    init

---
+++ src/test/java/br/com/helison/core/resource/NativeAdminResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.helison.core.resource;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeAdminResourceIT extends AdminResourceTest {
+
+    // Execute the same tests but in native mode.
+}
