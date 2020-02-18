cfbdfd211fb8eecc3f71a827e6dbf337367feb82
Helison Ortiz <helissoncavalheiro@gmail.com>
2020-02-17 23:05:31 -0300

    Removidas as classes padrão de testes

--- src/test/java/br/com/helison/core/resource/NativeAdminResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.helison.core.resource;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeAdminResourceIT extends AdminResourceTest {
-
-    // Execute the same tests but in native mode.
-}
