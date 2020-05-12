ae74258f983d895b5a2b8eb01f192c9dfe447e64
Helison Ortiz <helisson.cavalheiro@gmail.com>
2020-05-12 01:08:52 -0300

    Adiciona o retorno de apenas um serviço
    Permite que seja retornado apenas um determinado serviço.
    
    Para isso foi criado um endpoint `GET /servico/{id}` que espera um ID e retorna o serviço correspondente.
    
    Problemas:
    
        * Não há tratamento de possíveis exceções.
    
    [Ticket: HIS003]

--- src/main/java/br/com/helison/core/resource/ServicoResource.java
+++ src/main/java/br/com/helison/core/resource/ServicoResource.java
@@ -7,15 +7,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
 import br.com.helison.core.models.Servico;
 
 @Path("/servico")
 public class ServicoResource {
-    
+
     @GET
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Servico> getAllServicos(){
+    public List<Servico> getAllServicos() {
         return Servico.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Servico getServicoById(@PathParam Integer id) {
+        return Servico.findById(id);
     }
 }
