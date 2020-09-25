b1361f3c9f4e119a7aae25f07039ed99d685456c
Helison Ortiz <helisson.cavalheiro@gmail.com>
2020-09-25 11:18:11 -0300

    Remove apenas o conteúdo do token.
    Foi necessário manter a variável como uma string vazia para não acontecer imcompatibilidades em outras classes
    
    [Ticket: HIS007]

--- src/main/java/br/com/helison/core/utils/Constantes.java
+++ src/main/java/br/com/helison/core/utils/Constantes.java
@@ -3,6 +3,7 @@ package br.com.helison.core.utils;
 public interface Constantes {
 
     String URL_ISSUE = "https://demandas.urcamp.edu.br/issues/";
+    String TOKEN = "";
     String PAREMETRO_API_TOKEN = "X-Redmine-API-Key";
     String ACTIONS = "?include=journals&&status_id=*";
     String TYPE = ".json";
